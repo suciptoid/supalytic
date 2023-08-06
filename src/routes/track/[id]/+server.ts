1255eab976e51293597bbe1729361120686f9879
Sucipto <sucipto@pm.me>
2023-08-06 21:45:02 +0700

    cors on post method

--- src/routes/track/[id]/+server.ts
+++ src/routes/track/[id]/+server.ts
@@ -1,5 +1,11 @@
 import type { RequestHandler } from '@sveltejs/kit';
 
+const headers = {
+  'Access-Control-Allow-Methods': 'POST, OPTIONS',
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': '*'
+};
+
 export const POST = (async ({ url, params, request, locals: { db } }) => {
   try {
     const data = await request.json();
@@ -16,19 +22,13 @@ export const POST = (async ({ url, params, request, locals: { db } }) => {
     console.log('error receiveing track data');
   }
   return new Response('ok', {
-    status: 201
+    status: 201,
+    headers
   });
 }) satisfies RequestHandler;
 
 export const OPTIONS = (async () => {
   return new Response(null, {
-    headers: {
-      'Access-Control-Allow-Methods': 'GET, PUT, PATCH, POST, OPTIONS, HEAD',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': '*'
-    }
+    headers
   });
 }) satisfies RequestHandler;
-
-export const HEAD = OPTIONS;
-export const GET = OPTIONS;
