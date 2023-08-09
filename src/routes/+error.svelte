7749d9e843dbb5426c3db7684d60f4e46efe0447
Sucipto <sucipto@pm.me>
2023-08-09 18:29:30 +0700

    handle error message

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { page } from '$app/stores';
+</script>
+
+<svelte:head>
+  <title>Error {$page.status} | Supalytic</title>
+</svelte:head>
+<div class="flex min-h-screen w-full flex-col items-center justify-center">
+  <div class="p-10 text-center">
+    <h1 class="text-4xl font-extrabold text-gray-400">{$page.status}</h1>
+    <p class="text-gray-500">{$page.error?.message}</p>
+    <a href="/" class="py-4 text-sm font-semibold text-blue-500 hover:text-blue-600">Go Home</a>
+  </div>
+</div>
--- src/routes/dashboard/website/[id]/+page.server.ts
+++ src/routes/dashboard/website/[id]/+page.server.ts
@@ -1,13 +1,29 @@
+import { error, fail } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const load = (async ({ params, locals: { db } }) => {
-  const { data: browsers, error } = await db.rpc('get_metrics', {
+export const load = (async ({ params, locals: { db, getSession } }) => {
+  const session = await getSession();
+  const { data: website } = await db
+    .from('website_users')
+    .select('role,websites!inner(id,domain)')
+    .eq('website_id', params.id)
+    .eq('user_id', session?.user.id)
+    .single();
+
+  if (!website) {
+    throw error(404, {
+      message: 'Website not found'
+    });
+  }
+
+  console.log('website data', website);
+  const { data: browsers } = await db.rpc('get_metrics', {
     website_id: params.id,
     metric_name: 'browser',
     range: '1 hour'
   });
 
-  console.log('site browsers', browsers);
+  // console.log('site browsers', browsers);
 
   return {
     browsers
