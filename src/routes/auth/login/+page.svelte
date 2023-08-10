a3d677b84bf16000dcb2e56080fb12816677d7d0
andreecy <andreechristianyulianto@gmail.com>
2023-08-10 21:55:39 +0700

    login ui styling, logout

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -9,16 +9,16 @@
   <title>Supalytic | Login</title>
 </svelte:head>
 
-<div class="flex min-h-screen w-full items-center justify-center">
-  <div class="flex-center flex w-full max-w-lg">
+<div class="flex min-h-screen items-center justify-center bg-black/90 font-dashsans text-gray-400">
+  <div class="flex w-full max-w-md flex-col">
+    <p class="text-center text-2xl font-semibold text-primary-500 mb-4">Supalytic</p>
     <!-- <form action="">
 
 			<fieldset>
 				<input type="email" class="px-3 py-2 rounded-md" placeholder="Your email">
 			</fieldset>
 		</form> -->
-    <div class="login mx-auto">
-      <!-- <Auth
+    <!-- <Auth
 				additionalData={data}
 				supabaseClient={data.supabase}
 				view="magic_link"
@@ -26,6 +26,7 @@
 				showLinks={false}
 				appearance={{ theme: ThemeSupa, style: { input: 'color: #fff' } }}
 			/> -->
+    <div class="flex-1">
       <Auth
         additionalData={data}
         supabaseClient={data.supabase}
