b09a02690f83f53ff98d0fdd587b8519bd496764
andreecy <andreechristianyulianto@gmail.com>
2023-08-12 21:21:42 +0700

    slay slate slate :D

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -11,7 +11,7 @@
     {#each data.websites as website}
       <a
         href={`/dashboard/website/${website.id}`}
-        class="flex flex-col items-start rounded-md bg-white p-6 transition hover:border-gray-500/40 hover:bg-gray-400/10 dark:bg-gray-600/10 dark:bg-slate-800 dark:hover:bg-gray-400/10"
+        class="flex flex-col items-start rounded-md bg-white p-6 transition hover:border-gray-500/40 hover:bg-gray-400/10 dark:bg-gray-600/10 dark:hover:bg-gray-400/10"
       >
         <div class="font-medium">{website.name}</div>
         <span class="flex items-center space-x-1 text-sm text-gray-500 hover:text-blue-500">
@@ -24,7 +24,7 @@
   <a
     id="add-website"
     href="/dashboard/add"
-    class="flex h-[90px] items-center justify-center rounded-md border border-dashed border-gray-500/20 bg-white p-6 text-center text-sm font-medium text-gray-600 transition hover:border-gray-500/40 hover:bg-gray-400/10 dark:bg-gray-600/10 dark:bg-slate-800 dark:hover:bg-gray-400/10"
+    class="flex h-[90px] items-center justify-center rounded-md border border-dashed border-gray-500/20 bg-white p-6 text-center text-sm font-medium text-gray-600 transition hover:border-gray-500/40 hover:bg-gray-400/10 dark:bg-gray-600/10 dark:hover:bg-gray-400/10"
   >
     <Icon icon="mdi:add" width={16} height={16} />
     Add Website</a
